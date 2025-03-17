f123b4620f1566be836f3faa56c34eae8f050800
CelestialKittieAnnie <111916305+CelestialKittieAnnie@users.noreply.github.com>
2025-03-17 13:19:52 -0700

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "42365bc3-e31d-4d27-8f5a-f6f9fb66fdcd");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
